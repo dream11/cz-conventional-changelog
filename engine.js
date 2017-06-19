54bf4100830403cfd5c11e974a887f3468d90f35
Shwet Solanki <shwet@dream11.com>
2017-06-19 16:40:27 +0530

    Git description updated

--- engine.js
+++ engine.js
@@ -7,25 +7,25 @@ var rightPad = require('right-pad');
 
 var filter = function(array) {
     return array.filter(function(x) {
-        return x;
-    });
+                        return x;
+                        });
 };
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
 module.exports = function(options) {
-
+    
     var types = options.types;
-
+    
     var length = longest(Object.keys(types)).length + 1;
     var choices = map(types, function(type, key) {
-        return {
-            name: rightPad(key + ':', length) + ' ' + type.description,
-            value: key
-        };
-    });
-
+                      return {
+                      name: rightPad(key + ':', length) + ' ' + type.description,
+                      value: key
+                      };
+                      });
+    
     return {
         // When a user runs `git cz`, prompter will
         // be executed. We pass you cz, which currently
@@ -38,69 +38,69 @@ module.exports = function(options) {
         //
         // By default, we'll de-indent your commit
         // template and will keep empty lines.
-        prompter: function(cz, commit) {
-            console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
-
-            // Let's ask some questions of the user
-            // so that we can populate our commit
-            // template.
-            //
-            // See inquirer.js docs for specifics.
-            // You can also opt to use another input
-            // collection library if you prefer.
-            cz.prompt([{
-                    type: 'list',
-                    name: 'type',
-                    message: 'Select the type of change that you\'re committing:',
-                    choices: choices
-                }, {
-                    type: 'input',
-                    name: 'scope',
-                    message: 'Denote the scope of this change ($location, $browser, $compile, etc.):\n'
-                }, {
-                    type: 'input',
-                    name: 'subject',
-                    message: 'Write a short, imperative tense description of the change:\n'
-                }, {
-                    type: 'input',
-                    name: 'body',
-                    message: 'Provide a longer description of the change:\n'
-                }, {
-                    type: 'input',
-                    name: 'issues',
-                    message: 'Jira Issue ID(s) (required):\n'
-                }, {
-                    type: 'input',
-                    name: 'workflow',
-                    message: 'Workflow command (testing, closed, etc.) (optional):\n'
-                }
-                // , {
-                //   type: 'input',
-                //   name: 'comment',
-                //   message: 'Jira comment (optional):\n'
-                // }
-            ]).then(function(answers) {
-                // parentheses are only needed when a scope is present
-                var scope = answers.scope.trim();
-                scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
-                // Hard limit this line
-                var head = (answers.type + scope + ': ' + answers.subject.trim());
-
-                // Wrap these lines at 100 characters
-                var body = answers.body;
-
-                var issues = filter([
-                    answers.issues,
-                    answers.workflow ? '#' + answers.workflow : undefined,
-                    answers.time ? '#time ' + answers.time : undefined,
-                ]).join(' ');
-
-                // var footer = filter([answers.head, answers.body, issues]).join('\n\n');
-
-                commit(filter([head, answers.body, issues]).join(' '));
-                // commit(head + '\n\n' + body + '\n\n' + footer);
-            });
-        }
+    prompter: function(cz, commit) {
+        console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+        
+        // Let's ask some questions of the user
+        // so that we can populate our commit
+        // template.
+        //
+        // See inquirer.js docs for specifics.
+        // You can also opt to use another input
+        // collection library if you prefer.
+        cz.prompt([{
+                   type: 'list',
+                   name: 'type',
+                   message: 'Select the type of change that you\'re committing:',
+                   choices: choices
+                   }, {
+                   type: 'input',
+                   name: 'scope',
+                   message: 'Denote the scope of this change ($location, $browser, $compile, etc.):\n'
+                   }, {
+                   type: 'input',
+                   name: 'subject',
+                   message: 'Write a short, imperative tense description of the change:\n'
+                   }, {
+                   type: 'input',
+                   name: 'body',
+                   message: 'Provide a longer description of the change:\n'
+                   }, {
+                   type: 'input',
+                   name: 'issues',
+                   message: 'Jira Issue ID(s) (required):\n'
+                   }, {
+                   type: 'input',
+                   name: 'workflow',
+                   message: 'Workflow command (testing, closed, etc.) (optional):\n'
+                   }
+                   // , {
+                   //   type: 'input',
+                   //   name: 'comment',
+                   //   message: 'Jira comment (optional):\n'
+                   // }
+                   ]).then(function(answers) {
+                           // parentheses are only needed when a scope is present
+                           var scope = answers.scope.trim();
+                           scope = scope ? '(' + answers.scope.trim() + ')' : '';
+                           
+                           // Hard limit this line
+                           var head = (answers.type + scope + ': ' + answers.subject.trim());
+                           
+                           // Wrap these lines at 100 characters
+                           var body = answers.body;
+                           
+                           var issues = filter([
+                                                answers.issues,
+                                                answers.workflow ? '#' + answers.workflow : undefined,
+                                                answers.time ? '#time ' + answers.time : undefined,
+                                                ]).join(' ');
+                           
+                           // var footer = filter([answers.head, answers.body, issues]).join('\n\n');
+                           
+                           commit(filter([head, issues]).join(' ') + '\n\n' + answers.body);
+                           // commit(head + '\n\n' + body + '\n\n' + footer);
+                           });
+    }
     };
 };
